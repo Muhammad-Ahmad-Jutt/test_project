5d96910cf8bc9be4bc977294aa8ade41c61c1333
Ahmad Arshad <ahmadarshad12345@gmail.com>
2026-04-21 16:14:09 +0100

    presentation

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Dockerized Flask!"
+    return "Hello from Dockerized application Flask!"
 
 @app.route("/health")
 def health():
