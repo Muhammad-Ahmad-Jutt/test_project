a0b3901d7ca009554c218aec3ef34ef36ccb0770
Ahmad Arshad <ahmadarshad12345@gmail.com>
2026-04-21 16:17:52 +0100

    automatice deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Dockerized application Flask!"
+    return "Hello from Dockerized Flask!"
 
 @app.route("/health")
 def health():
